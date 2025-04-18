ca56ff0f3a64a164a0c368c1a9bc605800de0856
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-17 21:25:56 -0800

    Domain Foulder

---
+++ DY.Domain/CourseCategory.Agg/CourseCategory.cs
@@ -0,0 +1,16 @@
+﻿
+using _0_Framework.Domain;
+using DoreYab.Domain.CourseAgg;
+
+namespace DoreYab.Domain.CourseCategoryAgg
+{
+    public class CourseCategory : EntityBase
+    {
+        public string Title { get;private set; }
+        public string? ShortDescription { get; private set; }
+        public long CourseCount { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        public ICollection<Course> Courses { get;private set; }
+    }
+}
---
+++ DY.Domain/CourseCategory.Agg/ICourseCategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace DoreYab.Domain.CourseCategoryAgg
+{
+    public interface ICourseCategoryRepository
+    {
+        List<CourseCategory> GetAll();
+        void Create(CourseCategory entity);
+    }
+}
