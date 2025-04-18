55399cf68bea8f2ffbe467a0c51c1acce51fa050
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-17 21:26:11 -0800

    add contracts

---
+++ DY.Application.Contract/CourseCategory/ICourseCategoryApplication.cs
@@ -0,0 +1,14 @@
+﻿using DoreYab.Application.Contract.CourseCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DY.Application.Contract.CourseCategory
+{
+    public  interface ICourseCategoryApplication
+    {
+        List<CourseCategoryViewModel> List();
+    }
+}
