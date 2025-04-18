d8628519d4b8ad9bb7c41b09dd41c4f4ba726a4d
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-17 21:24:02 -0800

    Mapp Model and Entities with Fluent Api

---
+++ DY.Inferastructure.EfCore/Mapping/CourseConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using DoreYab.Domain.CourseAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoreYab.Inferastracture.Efcore.Mapping
+{
+    internal class CourseConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.ToTable(nameof(Course));
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Title).IsRequired().HasMaxLength(150);
+            builder.Property(c => c.SiteSource).IsRequired();
+            builder.Property(c => c.Desctiption);
+            builder.Property(c => c.ImageUrl);
+            builder.Property(c => c.IsFinished).IsRequired();
+            builder.Property(c => c.Slug).IsRequired();
+            builder.Property(c => c.Price);
+            builder.Property(c => c.MetaTitle);
+            builder.Property(c => c.MetaDescription);
+            builder.Property(c => c.MetaKeyword);
+
+            builder.HasOne(c => c.Category).WithMany(b => b.Courses).HasForeignKey(b => b.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
