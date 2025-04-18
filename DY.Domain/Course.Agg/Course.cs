7ae3771c83c62e9a0e4b6e1ba8e0be858f9c317d
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-17 21:24:17 -0800

    Course Model

---
+++ DY.Domain/Course.Agg/Course.cs
@@ -0,0 +1,49 @@
+﻿using _0_Framework.Domain;
+using DoreYab.Domain.CourseCategoryAgg;
+
+namespace DoreYab.Domain.CourseAgg
+{
+    public class Course : EntityBase
+    {
+        public string Title { get; private set; }
+        public decimal? Price { get; private set; }
+        public string? Desctiption { get; private set; }
+        public string? CourseUrl { get; private set; }
+        public string SiteSource { get; private set; }
+        public string Slug { get; private set; }
+        public string ImageUrl { get; private set; }
+        public bool IsFree { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public bool IsFinished { get; private set; }
+
+        // seo
+        public string MetaTitle { get; private set; }
+        public string MetaDescription { get; private set; }
+        public string MetaKeyword { get; private set; }
+
+        // Navigate
+        public CourseCategory Category { get; private set; }
+        public long CategoryId { get; private set; }
+
+
+        protected Course() { }
+
+        public Course(string title, decimal? price, string? desctiption, string? courseUrl, string siteSource, string slug, string imageUrl, string metaTitle, string metaDescription, string metaKeyword, long categoryId)
+        {
+            Title = title;
+            Price = price;
+            Desctiption = desctiption;
+            CourseUrl = courseUrl;
+            SiteSource = siteSource;
+            Slug = slug;
+            ImageUrl = imageUrl;
+            MetaTitle = metaTitle;
+            MetaDescription = metaDescription;
+            MetaKeyword = metaKeyword;
+            IsDeleted = false;
+            IsFree = true;
+            IsFinished = true;
+            CategoryId = categoryId;
+        }
+    }
+}
