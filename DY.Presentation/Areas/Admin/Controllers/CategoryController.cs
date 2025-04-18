aafd592d40bd8b26a00f0b7e6b7abd904d0f8e2b
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-18 03:52:20 -0800

    Create MVC Project

---
+++ DY.Presentation/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using DoreYab.Application.Contract.CourseCategory;
+using DY.Application.Contract.CourseCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DY.Presentation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        public List<CourseCategoryViewModel> CourseCategories { get; set; }
+        private readonly ICourseCategoryApplication _categoryApplication;
+
+        public CategoryController(ICourseCategoryApplication categoryApplication)
+        {
+            _categoryApplication = categoryApplication;
+        }
+
+        public IActionResult List()
+        {
+            CourseCategories = _categoryApplication.List();
+            return View(CourseCategories);
+        }
+
+    }
+}
