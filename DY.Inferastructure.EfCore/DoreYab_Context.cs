8a41c4173f37866a2002eecad694db01dca7147e
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-17 21:23:28 -0800

    DbContext and Use EfCore to Connection Sql Server

---
+++ DY.Inferastructure.EfCore/DoreYab_Context.cs
@@ -0,0 +1,24 @@
+﻿using DoreYab.Domain.CourseAgg;
+using DoreYab.Domain.CourseCategoryAgg;
+using DoreYab.Inferastracture.Efcore.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoreYab.Inferastracture.Efcore
+{
+    public class DoreYabContext : DbContext
+    {
+        public DbSet<CourseCategory> courseCategories { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public DoreYabContext(DbContextOptions<DoreYabContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CourseCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new CourseConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
